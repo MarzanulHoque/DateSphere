aa90919cc98b481751b003228af6e0a4f46e9d5c
Marzanul Swazan <marzanulru17@gmail.com>
2025-11-12 21:46:31 +0600

    Navbar Updated and profile edit button added.

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../core/services/member-service';
 
 @Component({
   selector: 'app-member-profile',
@@ -13,6 +14,7 @@ export class MemberProfile implements OnInit{
 
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
+  protected memberService = inject(MemberService);
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe( data => 
